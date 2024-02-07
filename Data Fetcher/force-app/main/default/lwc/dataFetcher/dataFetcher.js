3347789e1a03e1ebec4e403745e987d23538c993
cadillacben <cadillacben@gmail.com>
2024-02-07 09:50:13 -0500

    Update dataFetcher.js
    added sharing and field access options

--- Data Fetcher/force-app/main/default/lwc/dataFetcher/dataFetcher.js
+++ Data Fetcher/force-app/main/default/lwc/dataFetcher/dataFetcher.js
@@ -1,26 +1,30 @@
 /**
  * @description       : 
  * @author            : Josh Dayment
- * @group             : 
- * @last modified on  : 07-05-2023
- * @last modified by  : Josh Dayment
+ * @group             : https://github.com/SalesforceLabs/Data-Fetcher/tree/main
+ * @last modified on  : 2/6/23
+ * @last modified by  : Ben Patterson
 **/
 import { api,track, LightningElement } from "lwc";
-import getSObjects from "@salesforce/apex/DataFetcherController.getSObjects";
-import getAggregate from "@salesforce/apex/DataFetcherController.getAggregate";
+import getSObjects from "@salesforce/apex/dataFetcherController.getSObjects";
+import getAggregate from "@salesforce/apex/dataFetcherController.getAggregate";
+import getSObjectsWOsharing from "@salesforce/apex/dataFetcherWOsharingController.getSObjects";
+import getAggregateWOsharing from "@salesforce/apex/dataFetcherWOsharingController.getAggregate";
 import { FlowAttributeChangeEvent } from "lightning/flowSupport";
 
-export default class DataFetcher extends LightningElement {
+export default class dataFetcher extends LightningElement {
   @api queryString;
   @api aggQueryString;
   @api aggQueryResult;
+  @api withSharing =false;
+  @api systemAccess=false;
   @api firstRetrievedRecord;
   @api retrievedRecords = [];
   @api error;
   @track oldQuery;
   @track oldAggQuery;
 
-
+  
   renderedCallback() {
     if (this.queryString && this.queryString != this.oldQuery) {
     this._getRecords();}
@@ -35,10 +39,10 @@ export default class DataFetcher extends LightningElement {
   }
 
   _getRecords() {
-    
-    console.log("Query String is " + this.queryString)
-    
-      getSObjects({ queryString: this.queryString })
+    if (!this.withSharing){
+
+    //run in system mode  
+    getSObjectsWOsharing({ queryString: this.queryString , systemAccess: this.systemAccess})
         .then(({ results, firstResult }) => {
           this.error = undefined;
           this.retrievedRecords = results;
@@ -52,14 +56,30 @@ export default class DataFetcher extends LightningElement {
           this._fireFlowEvent("error", this.error);});
 
         this.oldQuery = this.queryString;
-    
+          } else{
+            //run in user mode  
+            getSObjects({ queryString: this.queryString , systemAccess: this.systemAccess })
+                .then(({ results, firstResult }) => {
+                  this.error = undefined;
+                  this.retrievedRecords = results;
+                  this.firstRetrievedRecord = firstResult;
+                  this._fireFlowEvent("firstRetrievedRecord", this.firstRetrievedRecord);
+                  this._fireFlowEvent("retrievedRecords", this.retrievedRecords);
+                })
+                .catch(error => 
+                  {this.error = error?.body?.message ?? JSON.stringify(error);
+                  console.error(error.body.message);
+                  this._fireFlowEvent("error", this.error);});
+        
+                this.oldQuery = this.queryString;    
+          }
   }
 
   _getAggregate() {
     
     //console.log("Query String is " + this.aggQueryString)
-    
-      getAggregate({ aggQueryString: this.aggQueryString })
+    if (!this.withSharing){
+      getAggregateWOsharing({ aggQueryString: this.aggQueryString, systemAccess: this.systemAccess } )
         .then(({ aggAmount, }) => {
           this.error = undefined;
           this.aggQueryResult = aggAmount;
@@ -71,7 +91,20 @@ export default class DataFetcher extends LightningElement {
           this._fireFlowEvent("error", this.error);});
 
         this.oldAggQuery = this.aggQueryString;
+          }   else{
+            getAggregate({ aggQueryString: this.aggQueryString, systemAccess: this.systemAccess } )
+            .then(({ aggAmount, }) => {
+              this.error = undefined;
+              this.aggQueryResult = aggAmount;
+              this._fireFlowEvent("aggQueryResult", this.aggQueryResult);
+            })
+            .catch(error => 
+              {this.error = error?.body?.message ?? JSON.stringify(error);
+              console.error(error.body.message);
+              this._fireFlowEvent("error", this.error);});
     
+            this.oldAggQuery = this.aggQueryString;
+          }
   }
 
   _debounceGetRecords() {
